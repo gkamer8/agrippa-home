62a072e0ced98bf26b1c82f1d3bbdc7b8b8a66ff
Gordon Kamer <gkamer@outlook.com>
2023-01-10 15:38:18 -0500

    coming soon page

---
+++ home/src/Home.js
@@ -0,0 +1,22 @@
+import './Home.css'
+
+function Home(props){
+    return (
+        <div className='home_container'>
+            <div className="main">
+                <h1>
+                    Coming soon.
+                </h1>
+                <div>
+                    Analyze, compose, and share complex AI models.
+                </div>
+                <br/>
+                <div>
+                    Contact <a href='mailto::team@agrippa.build'>team@agrippa.build</a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
---
+++ home/src/NavBar.js
@@ -0,0 +1,12 @@
+import './NavBar.css'
+
+function NavBar(props){
+
+    return (
+        <div className='navbar_container'>
+            
+        </div>
+    )
+}
+
+export default NavBar
