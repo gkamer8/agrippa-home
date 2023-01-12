256c658819bb4b45cf3cf9d2e57a3d2ea1c9e25c
Gordon Kamer <gkamer@college.harvard.edu>
2023-01-12 15:47:56 -0500

    type animation added

--- home/src/Home.js
+++ home/src/Home.js
@@ -1,18 +1,49 @@
 import './Home.css'
+import { TypeAnimation } from 'react-type-animation'
 
 function Home(props){
     return (
         <div className='home_container'>
             <div className="main">
                 <h1>
-                    Coming soon.
+                    <TypeAnimation
+                        sequence={[
+                            'Coming soon.',
+                            () => {
+                                console.log('Done typing!'); // Place optional callbacks anywhere in the array
+                            }
+                        ]}
+                        wrapper="div"
+                        cursor={false}
+                        repeat={0}
+                    />
                 </h1>
                 <div>
-                    Analyze, compose, and share complex AI models.
+                    <TypeAnimation
+                        sequence={[
+                            1000,
+                            'Analyze, compose, and share complex AI models.',
+                            () => {
+                                console.log('Done typing!'); // Place optional callbacks anywhere in the array
+                            }
+                        ]}
+                        cursor={false}
+                        repeat={Infinity}
+                    />
                 </div>
                 <br/>
                 <div>
-                    Contact <a href='mailto::team@agrippa.build'>team@agrippa.build</a>
+                    <TypeAnimation
+                        sequence={[
+                            4000,
+                            'Contact team@agrippa.build',
+                            () => {
+                                console.log('Done typing!'); // Place optional callbacks anywhere in the array
+                            }
+                        ]}
+                        cursor={false}
+                        repeat={Infinity}
+                    />
                 </div>
             </div>
         </div>
